a2a3b520e6d290b3cbd09f54fee37c2a8fad51d0
jzivic <51597154+jzivic@users.noreply.github.com>
2022-01-04 18:06:51 +0100

    0401-1

--- 1_Prepare/DirectoryWalk.py
+++ 1_Prepare/DirectoryWalk.py
@@ -1,8 +1,9 @@
+import collections
 import os
 
 # import numpy as np
 # import pandas as pd
-
+import pandas as pd
 
 from SimulationsData import *
 
@@ -31,30 +32,56 @@ class FSG_Analysis:
 
     def __init__(self):
 
+        # Podaci svih simulacija i svih vremenskih koraka
+        all_simulations_data_df = pd.DataFrame({"timeSteps":[[3,3]],
+                                                "inner_contours":[1],
+                                                "z":[1],
+                                                "outer_contours":[1],
+                                                "ILT_contours":[2]},
+                                            index= ["pero"])
+
+
+
+
+
+
+
         for simulation_folder in simulations:                               # ulazi u folder simulacije
             simulation_path = results_directory + simulation_folder
             os.chdir(simulation_path)
 
-
-
             # Setting simulation
             self.simulation_name = simulation_folder
+            self.n_sim = 0
+
             self.set_files()
-            self.d0 = float(self.whole_document_Inner_lines[5].strip().split()[0]) * 2
+            self.data_construction()
 
 
 
             for self.time_step in chosen_TimeSteps:
-                self.startLine_res_Inner_lines = 5 + TSLenght_res_Inner_lines * (self.time_step - 1)
-                self.startLine_res_ILT_lines = self.startLine_res_Inner_lines
-                self.startLine_res_Outer_lines = self.startLine_res_Inner_lines
+                self.n_sim += 1
+
+                self.setting_start_lines()
+                # self.check_AAA_formation()
+
+                self.timeStep_extraction()
 
 
-                self.check_AAA_formation()
 
-                self.Inner_lines()
+            # all_simulations_data_df.index.append(self.simulation_name)
+
+            # all_simulations_data_df["timeSteps"].add(2)
+
+            # all_simulations_data_df["inner_contours"].add(self.oneSim_data_dict["inner_contours"])
 
 
+            print(all_simulations_data_df)
+
+
+
+
+    # Samo jednom se postavlja
 
     def set_files(self):
         opening_res_NODE_0841 = open("res__NODE_0841_89-5", "r")  # open txt file
@@ -67,32 +94,70 @@ class FSG_Analysis:
         self.whole_document_Inner_lines = opening_res_Inner_lines.readlines()  # whole txt read in self.wholeDocument_eIW
         self.nl_res_Inner_lines = sum(1 for line in open("res__INNER_lines__89-5"))  # number of lines in export Inner Wall
 
+        opening_res_Outer_lines = open("res__OUTER_lines__89-5", "r")  # open txt file
+        self.whole_document_Outer_lines = opening_res_Outer_lines.readlines()  # whole txt read in self.wholeDocument_eIW
+        self.nl_res_Outer_lines = sum(1 for line in open("res__OUTER_lines__89-5"))  # number of lines in export Inner Wall
+
 
         opening_res_ILT_lines = open("res__ILT_lines__89-5", "r")  # open txt file
         self.whole_document_ILT_lines = opening_res_ILT_lines.readlines()  # whole txt read in self.wholeDocument_eIW
         self.nl_res_ILT_lines = sum(1 for line in open("res__ILT_lines__89-5"))  # number of lines in export Inner Wall
 
 
+    def data_construction(self):
+        self.d0 = float(self.whole_document_Inner_lines[5].strip().split()[0]) * 2
+
+
+        self.oneSim_data_dict = {"timeSteps":[], "inner_contours":[], "z":[], "outer_contours":[], "ILT_contours":[]}
+
+
+
+
+
+
+        #
+
+    # Svaki vremenski korak se vrti
+
+    def setting_start_lines(self):
+        self.startLine_res_Inner_lines = 5 + TSLenght_res_Inner_lines * (self.time_step - 1)
+        self.startLine_res_ILT_lines = self.startLine_res_Inner_lines
+        self.startLine_res_Outer_lines = self.startLine_res_Inner_lines
 
     def check_AAA_formation(self):
         for line in self.whole_document_Inner_lines[self.startLine_res_Inner_lines:
-                    (self.startLine_res_Inner_lines + TSLenght_res_Inner_lines -1)]:
+        (self.startLine_res_Inner_lines + TSLenght_res_Inner_lines -1)]:
             D = float(line.strip().split()[0]) * 2
             if D > 1.1 * self.d0:                                               # kako ovo definirati - mijenja se?
                 return True
-
         return False
 
 
-    def Inner_lines(self):
-        
+    def timeStep_extraction(self):
+        d_inner_list, z_list, d_outer_list, d_ILT_list = [], [], [], []
         for n_line in range(TSLenght_res_Inner_lines-1):
 
-            d_ILT_cont = float(self.whole_document_ILT_lines[self.startLine_res_ILT_lines + n_line].strip().split()[0])*2
+            d_inner = float(self.whole_document_Inner_lines[self.startLine_res_Inner_lines + n_line].strip().split()[0])*2
+            z = float(self.whole_document_Inner_lines[self.startLine_res_Inner_lines + n_line].strip().split()[3])
+            d_outer = float(self.whole_document_Outer_lines[self.startLine_res_Outer_lines + n_line].strip().split()[0])*2
+            d_ILT = float(self.whole_document_ILT_lines[self.startLine_res_ILT_lines + n_line].strip().split()[0])*2
 
-            d_Inner_cont = float(self.whole_document_Inner_lines[self.startLine_res_Inner_lines + n_line].strip().split()[0])*2
+            d_inner_list.append(d_inner)
+            z_list.append(z)
+            d_outer_list.append(d_outer)
+            d_ILT_list.append(d_ILT)
 
-            print(d_Inner_cont)
+        self.oneSim_data_dict["inner_contours"].append(d_inner_list)
+        self.oneSim_data_dict["z"].append(z_list)
+        self.oneSim_data_dict["outer_contours"].append(d_outer_list)
+        self.oneSim_data_dict["ILT_contours"].append(d_ILT_list)
+        self.oneSim_data_dict["timeSteps"].append(self.time_step)
+
+
+
+
+
+
 
 
 FSG_Analysis()
--- 1_Prepare/SimulationsData.py
+++ 1_Prepare/SimulationsData.py
@@ -5,7 +5,7 @@ results_directory = "//home/josip/feap/FSG/automatizacija_8/"   #path to analysi
 
 simulations = ["prava_025_14", "prava_030_14", "prava_035_14", "prava_040_14" ]
 # chosen_TimeSteps = [i for i in range(100,300,5)]
-chosen_TimeSteps = [313]         # mora biti pozitivno
+chosen_TimeSteps = [1, 313]         # mora biti pozitivno
 
 
 simulations = ["prava_035_14"]
