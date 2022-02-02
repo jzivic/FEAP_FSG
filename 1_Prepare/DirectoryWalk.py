a9447d96ca8b9ab2c283479c78885f88451c374e
jzivic <51597154+jzivic@users.noreply.github.com>
2022-02-02 12:32:51 +0100

    0201-2

--- 1_Prepare/DirectoryWalk.py
+++ 1_Prepare/DirectoryWalk.py
@@ -1,27 +1,17 @@
 from SimulationsData import *
 
 
-import collections
 import os
-
-# import numpy as np
 import pandas as pd
 
 
-
-
-
-suffix_list = ["89-5"]
-
 TSLenght = 121
-
 TSLenght_res_Inner_lines = TSLenght + 1
 TSLlenght_res_Outer_lines = TSLenght + 1
 TSLlenght_res_ILT_lines = TSLenght + 1
 TSLlenght_rN0841 = 1
-
 TSLlenght_res_Y0__field = 850
-
+# suffix_list = ["89-5"]
 
 
 class FSG_Analysis:
@@ -31,15 +21,18 @@ class FSG_Analysis:
         # Podaci svih simulacija i svih vremenskih koraka
         all_simulations_data_df = pd.DataFrame({"timeStep":[],
                                                 "inner_contours":[],
-                                                "z":[],
-                                                "outer_contours":[],
-                                                "ILT_contours":[],
+                                                "outer_contours": [],
+                                                "ILT_contours": [],
+                                                "Z_contours":[],
                                                 "ILT_thickness_contours":[],
                                                 "vein_thickness_contours": [],
-                                                "D": [],
-                                                "H": [],
                                                 "S22_contours": [],
+
+                                                "H": [],
+                                                "D_max": [],
                                                 "S22_max": [],
+                                                "ILT_thickness_max": [],
+                                                "vein_thickness_max": [],
                                                 },
                                             index= [])
 
@@ -65,19 +58,24 @@ class FSG_Analysis:
                 elif self.check_AAA_formation() == False:
                     dopuna = [None for i in range(len(all_simulations_data_df))]
 
-
             dopuna = [self.oneSim_data_dict["timeStep"],
+
                       self.oneSim_data_dict["inner_contours"],
-                      self.oneSim_data_dict["z"],
                       self.oneSim_data_dict["outer_contours"],
                       self.oneSim_data_dict["ILT_contours"],
+                      self.oneSim_data_dict["Z_contours"],
+
                       self.oneSim_data_dict["ILT_thickness_contours"],
                       self.oneSim_data_dict["vein_thickness_contours"],
-                      self.oneSim_data_dict["D"],
-                      self.oneSim_data_dict["H"],
                       self.oneSim_data_dict["S22_contours"],
-                      self.oneSim_data_dict["S22_max"]
+
+                      self.oneSim_data_dict["H"],
+                      self.oneSim_data_dict["D_max"],
+                      self.oneSim_data_dict["S22_max"],
+                      self.oneSim_data_dict["ILT_thickness_max"],
+                      self.oneSim_data_dict["vein_thickness_max"]
                       ]
+
 
             all_simulations_data_df.loc[self.simulation_name] = dopuna
         all_simulations_data_df.to_pickle(pickle_name)
@@ -113,9 +111,13 @@ class FSG_Analysis:
 
     def data_construction(self):
         self.r0 = float(self.whole_document_Inner_lines[5].strip().split()[0])
-        self.oneSim_data_dict = {"timeStep":[], "inner_contours":[], "z":[], "outer_contours":[], "ILT_contours":[],
-                                 "ILT_thickness_contours":[], "vein_thickness_contours":[],"H":[], "D":[],
-                                 "S22_contours":[], "S22_max":[]}
+        self.oneSim_data_dict = {"timeStep":[], "inner_contours":[], "outer_contours":[], "ILT_contours":[],
+                                 "Z_contours":[], "ILT_thickness_contours":[], "vein_thickness_contours":[],
+                                 "S22_contours": [],
+
+                                 "H":[], "D_max":[],"S22_max":[], "ILT_thickness_max":[], "vein_thickness_max":[]
+
+                                 }
 
 
 
@@ -126,8 +128,7 @@ class FSG_Analysis:
         self.startLine_res_ILT_lines = self.startLine_res_Inner_lines
         self.startLine_res_Outer_lines = self.startLine_res_Inner_lines
 
-
-        # mogućnost odabira 1-7 radijalnog elementa, 1: skroz unutarnji
+        # mogućnost odabira 1-7 radijalnog elementa, 1: skroz unutarnji, 7: vanjski
         radial_layer = 7
         assert radial_layer in [1,2,3,4,5,6,7],  "Nedopušteni layer elementa"
         self.startLine_res_Y0_field = 139 + TSLlenght_res_Y0__field * self.time_step + ((radial_layer-1)*TSLenght)
@@ -143,6 +144,7 @@ class FSG_Analysis:
                 return True
         return False
 
+
     def timeStep_extraction(self):
         timeSteps_list, r_inner_list, z_list, r_outer_list, r_ILT_list = [], [], [], [], []
         ILT_thickness_list, vein_thickness_list, h_list, S22_list = [], [], [], [],
@@ -150,45 +152,50 @@ class FSG_Analysis:
         for n_line in range(TSLenght_res_Inner_lines-1):
 
             r_inner = float(self.whole_document_Inner_lines[self.startLine_res_Inner_lines + n_line].strip().split()[0])
-            z = float(self.whole_document_Inner_lines[self.startLine_res_Inner_lines + n_line].strip().split()[3])
             r_outer = float(self.whole_document_Outer_lines[self.startLine_res_Outer_lines + n_line].strip().split()[0])
             r_ILT = float(self.whole_document_ILT_lines[self.startLine_res_ILT_lines + n_line].strip().split()[0])
             ILT_thickness = r_inner - r_ILT
             vein_thickness = r_outer - r_inner
-
+            z = float(self.whole_document_Inner_lines[self.startLine_res_Inner_lines + n_line].strip().split()[3])
             S22 = float(self.whole_document_res_Y0_field[self.startLine_res_Y0_field+n_line].strip().split()[4])
 
             r_inner_list.append(r_inner)
-            z_list.append(z)
             r_outer_list.append(r_outer)
             r_ILT_list.append(r_ILT)
             ILT_thickness_list.append(ILT_thickness)
             vein_thickness_list.append(vein_thickness)
+            z_list.append(z)
             S22_list.append(S22)
 
             if r_inner > 1.05 * self.r0:
                 h_list.append(z)
 
         H = h_list[-1] - h_list[0]
-        D = max(r_inner_list)*2
+        D_max = max(r_inner_list)*2
         S22_max = max(S22_list)
+        ILT_thickness_max = max(ILT_thickness_list)
+        vein_thickness_max = max(vein_thickness_list)
+
 
 
         self.oneSim_data_dict["timeStep"].append(self.time_step)
 
         self.oneSim_data_dict["inner_contours"].append(r_inner_list)
-        self.oneSim_data_dict["z"].append(z_list)
         self.oneSim_data_dict["outer_contours"].append(r_outer_list)
         self.oneSim_data_dict["ILT_contours"].append(r_ILT_list)
+        self.oneSim_data_dict["Z_contours"].append(z_list)
 
         self.oneSim_data_dict["ILT_thickness_contours"].append(ILT_thickness_list)
         self.oneSim_data_dict["vein_thickness_contours"].append(vein_thickness_list)
-
-        self.oneSim_data_dict["D"].append(D)
-        self.oneSim_data_dict["H"].append(H)
-
         self.oneSim_data_dict["S22_contours"].append(S22_list)
+
+        self.oneSim_data_dict["H"].append(H)
+        self.oneSim_data_dict["D_max"].append(D_max)
         self.oneSim_data_dict["S22_max"].append(S22_max)
+
+        self.oneSim_data_dict["ILT_thickness_max"].append(ILT_thickness_max)
+        self.oneSim_data_dict["vein_thickness_max"].append(vein_thickness_max)
+
 
 
 
--- 1_Prepare/SimulationAnalysis.py
+++ 1_Prepare/SimulationAnalysis.py
@@ -37,11 +37,10 @@ def time_analysis(times):
                 continue
 
             index = all_data.loc[simul]["timeStep"].index(trenutak)
-
             inner_cont = all_data.loc[simul]["inner_contours"][index]
             ILT_cont = all_data.loc[simul]["ILT_contours"][index]
             outer_cont = all_data.loc[simul]["outer_contours"][index]
-            z = all_data.loc[simul]["z"][index]
+            Z_cont = all_data.loc[simul]["Z_contours"][index]
 
             ILT_thickness = all_data.loc[simul]["ILT_thickness_contours"][index]
             vein_thickness = all_data.loc[simul]["vein_thickness_contours"][index]
@@ -51,8 +50,8 @@ def time_analysis(times):
 
             def plot_contours():
                 color = next(plt.gca()._get_lines.prop_cycler)['color']
-                plt.plot(z, inner_cont, c=color, label=simul)
-                plt.plot(z, ILT_cont, linestyle=':', c=color)
+                plt.plot(Z_cont, inner_cont, c=color, label=simul)
+                plt.plot(Z_cont, ILT_cont, linestyle=':', c=color)
                 # plt.plot(z, outer_cont, linestyle='--', c=color)
                 plt.title(str(trenutak)+". korak")
                 plt.ylabel("Radius [mm]")
@@ -66,7 +65,7 @@ def time_analysis(times):
 
             def plot_stress():
                 color = next(plt.gca()._get_lines.prop_cycler)['color']
-                plt.plot(z, S22_cont, c=color, label=simul)
+                plt.plot(Z_cont, S22_cont, c=color, label=simul)
                 plt.title(str(trenutak)+". korak")
                 plt.ylabel("Stress S22 [mm]")
                 plt.xlabel("Axial coordinate $z$ [mm]")
@@ -78,7 +77,7 @@ def time_analysis(times):
 
             def debljina_stijenke():
                 color = next(plt.gca()._get_lines.prop_cycler)['color']
-                plt.plot(z, vein_thickness, linestyle='-', c=color, label=simul)
+                plt.plot(Z_cont, vein_thickness, linestyle='-', c=color, label=simul)
                 plt.title("Debljina stijenke: "+ str(trenutak) + ". korak")
                 plt.ylabel("Thickness [mm]")
                 plt.xlabel("Axial coordinate $z$ [mm]")
@@ -88,7 +87,7 @@ def time_analysis(times):
 
             def debljina_ILTa():
                 color = next(plt.gca()._get_lines.prop_cycler)['color']
-                plt.plot(z, ILT_thickness, c=color, label=simul)
+                plt.plot(Z_cont, ILT_thickness, c=color, label=simul)
                 plt.title("Debljina ILTa: "+ str(trenutak)+ ". korak")
                 plt.ylabel("Thickness [mm]")
                 plt.xlabel("Axial coordinate $z$ [mm]")
@@ -109,25 +108,24 @@ wanted_D = r*2
 def diameter_analysis():
     for simul in simulation_names:
 
-        nearest_diameter = min (all_data.loc[simul]["D"], key=lambda x: abs(x-wanted_D))         # najbliža vrijednost
-        index = list(all_data.loc[simul]["D"]).index(nearest_diameter)
+        nearest_diameter = min (all_data.loc[simul]["D_max"], key=lambda x: abs(x-wanted_D))         # najbliža vrijednost
+        index = list(all_data.loc[simul]["D_max"]).index(nearest_diameter)
         time = all_data.loc[simul]["timeStep"][index]
         print(time, simul)
 
         inner_cont = all_data.loc[simul]["inner_contours"][index]
         ILT_cont = all_data.loc[simul]["ILT_contours"][index]
         outer_cont = all_data.loc[simul]["outer_contours"][index]
-        z = all_data.loc[simul]["z"][index]
+        Z_cont = all_data.loc[simul]["Z_contours"][index]
         ILT_thickness = all_data.loc[simul]["ILT_thickness_contours"][index]
         vein_thickness = all_data.loc[simul]["vein_thickness_contours"][index]
         S22_cont = all_data.loc[simul]["S22_contours"][index]
 
 
-
         def plot_contours():
             color = next(plt.gca()._get_lines.prop_cycler)['color']
-            plt.plot(z, inner_cont, c=color, label=(simul+", TS: "+str(time)))
-            plt.plot(z, ILT_cont, linestyle=':', c=color)
+            plt.plot(Z_cont, inner_cont, c=color, label=(simul+", TS: "+str(time)))
+            plt.plot(Z_cont, ILT_cont, linestyle=':', c=color)
             # plt.plot(z, outer_cont, linestyle='--', c=color)
             # plt.title(str(trenutak) + ". korak")
             plt.ylabel("Radius [mm]")
@@ -136,12 +134,12 @@ def diameter_analysis():
             plt.xlim([0, 250])
             plt.grid(which='both', linestyle='--', linewidth='0.5')
             plt.legend()
-        # plot_contours()
+        plot_contours()
 
 
         def plot_stress():
             color = next(plt.gca()._get_lines.prop_cycler)['color']
-            plt.plot(z, S22_cont, c=color, label=(simul+", TS: "+str(time)))
+            plt.plot(Z_cont, S22_cont, c=color, label=(simul+", TS: "+str(time)))
             # plt.title(str(trenutak) + ". korak")
             plt.ylabel("Stress S22 [mm]")
             plt.xlabel("Axial coordinate $z$ [mm]")
@@ -153,7 +151,7 @@ def diameter_analysis():
 
         def debljina_stijenke():
             color = next(plt.gca()._get_lines.prop_cycler)['color']
-            plt.plot(z, vein_thickness, linestyle='-', c=color, label=(simul+", TS: "+str(time)))
+            plt.plot(Z_cont, vein_thickness, linestyle='-', c=color, label=(simul+", TS: "+str(time)))
             plt.title("Debljina stijenke: ")
             plt.ylabel("Thickness [mm]")
             plt.xlabel("Axial coordinate $z$ [mm]")
@@ -164,13 +162,13 @@ def diameter_analysis():
 
         def debljina_ILTa():
             color = next(plt.gca()._get_lines.prop_cycler)['color']
-            plt.plot(z, ILT_thickness, c=color, label=(simul+", TS: "+str(time)))
+            plt.plot(Z_cont, ILT_thickness, c=color, label=(simul+", TS: "+str(time)))
             plt.title("Debljina ILTa: ")
             plt.ylabel("Thickness [mm]")
             plt.xlabel("Axial coordinate $z$ [mm]")
             plt.grid(which='both', linestyle='--', linewidth='0.5')
             plt.legend()
-        debljina_ILTa()
+        # debljina_ILTa()
 
 
     plt.show()
@@ -178,22 +176,92 @@ def diameter_analysis():
 # diameter_analysis()
 
 
-
-# def growth_over_time()
-
+print(all_data.loc["AAA_Newt_snizena_85"])
 
 
-sim = all_data.loc["AAA_Newt_snizena_65"]
 
 
-print(sim)
+simulation_names = [
+                    "AAA_Newt_snizena_85",
+                    ]
 
-# plt.plot(sim["D"])
-# plt.show()
+
+def growth_over_time():
+    for simul in simulation_names:
+
+        timeStep = all_data.loc[simul]["timeStep"]
+        D_max = all_data.loc[simul]["D_max"]
+        H = all_data.loc[simul]["H"]
+
+        S22_max = all_data.loc[simul]["S22_max"]
+        ILT_thickness_max = all_data.loc[simul]["ILT_thickness_max"]
+        vein_thickness_max = all_data.loc[simul]["vein_thickness_max"]
 
 
-# for i in all_data:
-#     print(i)
+
+
+        def rast_D():
+            color = next(plt.gca()._get_lines.prop_cycler)['color']
+            plt.plot(timeStep, D_max, label=(simul))
+            plt.title("Rast D: ")
+            plt.ylabel("D [mm]")
+            plt.xlabel("timeStep [-]")
+            plt.grid(which='both', linestyle='--', linewidth='0.5')
+            plt.legend()
+        # rast_D()
+
+
+        def rast_H():
+            color = next(plt.gca()._get_lines.prop_cycler)['color']
+            plt.plot(timeStep, H, label=(simul))
+            plt.title("Rast H: ")
+            plt.ylabel("H [mm]")
+            plt.xlabel("timeStep [-]")
+            plt.grid(which='both', linestyle='--', linewidth='0.5')
+            plt.legend()
+        # rast_H()
+
+
+
+        def rast_S22():
+            color = next(plt.gca()._get_lines.prop_cycler)['color']
+            plt.plot(timeStep, S22_max, label=(simul))
+            plt.title("Rast S22: ")
+            plt.ylabel("S22_max [N]")
+            plt.xlabel("timeStep [-]")
+            plt.grid(which='both', linestyle='--', linewidth='0.5')
+            plt.legend()
+        rast_S22()
+
+
+
+
+        # plt.show()
+
+
+
+
+
+
+growth_over_time()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
